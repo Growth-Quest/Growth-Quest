046d55e37f1250679bbba47337f70e4514ab1139
Wisdomous20 <aljason.javier-20@cpu.edu.ph>
2024-05-26 13:52:11 +0800

    route auth fix -1

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,14 +2,26 @@ import jwt from 'jsonwebtoken';
 
 export const auth = (req: any, res: any, next: any) => {
   const authHeader = req.headers['authorization'];
-  // console.log("Token from middleware", authHeader);
+
+  if (!authHeader) {
+    return res.status(401).send('Access Denied: No Token Provided!');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send('Access Denied: Malformed Token!');
+  }
+
   try {
-    jwt.verify(authHeader, 'super secret key here', (err: any, user: any) => {
-      if (err) return res.status(403).send('Invalid Token');
+    jwt.verify(token, 'super secret key here', (err: any, user: any) => {
+      if (err) {
+        return res.status(403).send('Invalid Token');
+      }
       req.user = user;
-      next(); 
+      next();
     });
   } catch (error) {
     res.status(400).send('Invalid Token');
   }
-} 
+};
